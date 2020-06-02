1e8e820c48f0bc5445d3f4a7b52dc24db371c76d
luizhenriquelongo <longo.luizh@gmail.com>
2020-06-02 01:28:04 -0300

    update initialization file

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,19 @@ from app.database import db
 from app.website.routes import site_blueprints
 from app.api.v1 import api
 
-
-app = Flask(
-    __name__,
-    template_folder='website/templates',
-    static_folder='website/static'
-)
-app.config.from_object(Config)
-
-# Registering website routes
-for site_blueprint in site_blueprints:
-    app.register_blueprint(site_blueprint)
-
-api.init_app(app)
-db.init_app(app)
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder='website/templates',
+        static_folder='website/static'
+    )
+    app.config.from_object(Config)
+    
+    # Registering website routes
+    for site_blueprint in site_blueprints:
+        app.register_blueprint(site_blueprint)
+    
+    api.init_app(app)
+    db.init_app(app)
+    
+    return app
